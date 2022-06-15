6b6ec9ca3e446032a4c6bab8c5040b7bc12c5069
Bryan Paucarima Franco <>
2022-06-15 22:59:10 +0200

    change api hook

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,5 +1,7 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useTranslation } from "react-i18next";
+import { getMovieUpcoming } from "../../core/movies/movies.utils";
+import { useFetchMedia } from "../../hooks/useFetchMedia";
 import MostPopular from "./components-home/most-popular";
 import Trending from "./components-home/trending";
 import UpComing from "./components-home/up-coming";
@@ -8,37 +10,25 @@ import './styles.css'
 
 function Home() {
 
-  const [mostPopularMovies, setMostPopularMovies] = useState([])
+  // const [mostPopularMovies, setMostPopularMovies] = useState([])
   const [trendingMovies, setTrendingMovies] = useState([])
   const [upComingMovies, setUpComingMovies] = useState([])
   const {t} = useTranslation('global');
 
-  const API_KEY = process.env.REACT_APP_API_KEY_MOVIE_DB
+  const { data: mostPopularMovies, loading } = useFetchMedia('movie/upcoming', getMovieUpcoming);
 
-  console.log(API_KEY)
+  // useEffect(() => {
+  //   fetch(`https://api.themoviedb.org/3/trending/movie/week?api_key=${API_KEY}&language=es&region=ES`)
+  //     .then(res => res.json())
+  //     .then(data => setTrendingMovies(data.results))
+  // }, [])
 
-  useEffect(() => {
-    fetch(`https://api.themoviedb.org/3/movie/upcoming?api_key=${API_KEY}&language=es&region=ES`)
-      .then(res => res.json())
-      .then(data => setUpComingMovies(data.results))
+  // useEffect(() => {
+  //   fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=es&region=ES`)
+  //     .then(res => res.json())
+  //     .then(data => setMostPopularMovies(data.results))
+  // }, [])
 
-  }, [])
-
-  useEffect(() => {
-    fetch(`https://api.themoviedb.org/3/trending/movie/week?api_key=${API_KEY}&language=es&region=ES`)
-      .then(res => res.json())
-      .then(data => setTrendingMovies(data.results))
-  }, [])
-
-  useEffect(() => {
-    fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=es&region=ES`)
-      .then(res => res.json())
-      .then(data => setMostPopularMovies(data.results))
-  }, [])
-
-  
-
-  console.log(upComingMovies)
 
   return (
     <>
@@ -53,7 +43,7 @@ function Home() {
         </div>
       <h2>{t('header.MOST_POPULAR')}</h2>
       <div className="home-popular">
-        {mostPopularMovies.map(m => <MostPopular key={m.id} popular={m}></MostPopular>)}
+        {mostPopularMovies && (mostPopularMovies.slice(0,2)).map(m => <MostPopular key={m.id} popular={m}></MostPopular>)}
       </div>
       <h2>{t('header.TRENDING')}</h2>
       <div className="home-trending">

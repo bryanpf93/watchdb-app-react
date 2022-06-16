0cf76eca7d3a835812010f52c13bdd53fa458662
Bryan Paucarima Franco <>
2022-06-16 10:04:33 +0200

    multi-theming

--- src/App.js
+++ src/App.js
@@ -14,28 +14,30 @@ import Header from './components/header';
 import Footer from './components/footer';
 import Movies from './pages/movies';
 import Series from './pages/series';
+import { useContext } from 'react';
+import { ThemeContext } from './contexts/theme.context';
 
 
 function App() {
-
-  
-
+  const [theme] = useContext(ThemeContext);
 
   return (
     <BrowserRouter>
-      <Header></Header>
-      <Routes>
-        <Route path="/" element={<Home></Home>} /> {/* /*/}
-        <Route path='/movies' element={<Movies></Movies>}></Route>
-        <Route path='/series' element={<Series></Series>}></Route>
-        <Route path='/validate' element={<Validate/>}></Route>
-        <Route path='/user' element={ <RequireAuth><User/></RequireAuth>}></Route>
-        <Route path='/auth'>
-          <Route path='register' element={<Register/>}></Route>
-          <Route path='login' element={<Login/>}></Route>
-        </Route>
-      </Routes>
-      <Footer></Footer>
+      <div className={`app ${theme}`}>
+        <Header></Header>
+        <Routes>
+          <Route path="/" element={<Home></Home>} /> {/* /*/}
+          <Route path='/movies' element={<Movies></Movies>}></Route>
+          <Route path='/series' element={<Series></Series>}></Route>
+          <Route path='/validate' element={<Validate/>}></Route>
+          <Route path='/user' element={ <RequireAuth><User/></RequireAuth>}></Route>
+          <Route path='/auth'>
+            <Route path='register' element={<Register/>}></Route>
+            <Route path='login' element={<Login/>}></Route>
+          </Route>
+        </Routes>
+        <Footer></Footer>
+      </div>
   </BrowserRouter>
 
   );
--- src/contexts/theme.provider.jsx
+++ src/contexts/theme.provider.jsx
@@ -1,12 +1,14 @@
 import { useEffect, useState } from "react";
 import { ThemeContext } from "./theme.context";
 
+const STORAGE_KEY = 'key-theme';
 
-export const STORAGE_KEY = 'key-theme';
+function ThemeProvider({ children }) {
 
-function ThemeProvider({children}){
-
-    const [theme,setTheme] = useState(() => localStorage.getItem(STORAGE_KEY))
+    const [theme, setTheme] = useState(() => {
+        const stored = localStorage.getItem(STORAGE_KEY);
+        return stored ? stored : 'light';
+    })
 
     useEffect(() => {
         localStorage.setItem(STORAGE_KEY, theme)
@@ -14,7 +16,7 @@ function ThemeProvider({children}){
 
     return (
 
-        <ThemeContext.Provider value={[theme,setTheme]}>
+        <ThemeContext.Provider value={[theme, setTheme]}>
             {children}
         </ThemeContext.Provider>
     )
